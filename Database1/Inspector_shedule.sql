44a0efe260320f625abf42966b524f5b6151c2f3
Rick van der Sommen <38458997+rickvdsommen@users.noreply.github.com>
2018-10-30 12:09:43 +0100

    Revert "Merge branch 'Rick'"
    This reverts commit 3d56c92b2614ad0fd9b18d5203098e3dc43572ad, reversing
    changes made to 052e4e2455a3f56743763b527ddaa167c0ba6aae.

--- Database1/Inspector_shedule.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Inspector_shedule]
-(
-	[inspector_id] INT NOT NULL PRIMARY KEY, 
-    [date] DATE NULL, 
-    [available_from] DATETIME NULL, 
-    [available_until] DATETIME NULL
-)
