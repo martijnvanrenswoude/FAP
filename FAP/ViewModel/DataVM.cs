62b19dfb79d7521ec01f6cf16c54e2a8889b3a36
sjors37 <sjorshekerman@live.nl>
2018-10-30 11:14:44 +0100

    dikke pdf test

---
+++ FAP/ViewModel/DataVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAP.ViewModel
+{
+    public class DataVM
+    {
+        public string Name { get; set; }
+        public string DayOfEmployment { get; set; }
+
+        public DataVM(string Name, string DayOfEmployment)
+        {
+            this.Name = Name;
+            this.DayOfEmployment = DayOfEmployment;
+        }
+    }
+}
--- FAP/ViewModel/MainViewModel.cs
+++ FAP/ViewModel/MainViewModel.cs
@@ -1,14 +1,62 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
 
 namespace FAP.ViewModel
 {
-  
+
     public class MainViewModel : ViewModelBase
     {
-   
+
+        public ObservableCollection<DataVM> Data { get; set; }
+        public string PDFName { get; set; }
+        public ICommand Export { get; set; }
+
         public MainViewModel()
         {
-         
+            Data = new ObservableCollection<DataVM>();
+
+            Data.Add(new DataVM("Noah", "30-10-2018"));
+            Data.Add(new DataVM("Sem", "12-02-2018"));
+            Data.Add(new DataVM("Lucas", "06-02-2018"));
+            Data.Add(new DataVM("Finn", "12-08-2018"));
+            Data.Add(new DataVM("Daan", "24-09-2018"));
+            Data.Add(new DataVM("Levi", "16-11-2018"));
+            Data.Add(new DataVM("Milan", "02-05-2018"));
+            Data.Add(new DataVM("Bram", "19-04-2018"));
+            Data.Add(new DataVM("Luuk", "26-12-2018"));
+            Data.Add(new DataVM("Jesse", "04-07-2018"));
+            Data.Add(new DataVM("Liam", "28-08-2018"));
+            Data.Add(new DataVM("Mees", "25-01-2018"));
+            Data.Add(new DataVM("Thom", "06-11-2018"));
+            Data.Add(new DataVM("Sam", "13-08-2018"));
+            Data.Add(new DataVM("Thijs", "18-09-2018"));
+
+            Export = new RelayCommand(ExportToPDF);
+        }
+
+        private void ExportToPDF()
+        {
+            if (PDFName == null)
+                return;
+
+            Document pdfDocument = new Document();
+
+            PdfWriter.GetInstance(pdfDocument, new FileStream("C:\\Users\\sjors\\Desktop\\PDF\\" + PDFName + ".PDF", FileMode.Create));
+            pdfDocument.Open();
+            pdfDocument.Add(new Paragraph("Naam     Startdatum"));
+            foreach (var item in Data)
+            {
+                pdfDocument.Add(new Paragraph(item.Name + "     " + item.DayOfEmployment));
+            }
+            pdfDocument.Close();
         }
     }
 }
