8b8804d6d277a14e1d4f29c5819a1aadd13d837b
martijnvanrenswoude <mrenswou@avans.nl>
2018-10-30 12:19:20 +0100

    Edit FK
    Co-Authored-By: wesselvonderfuhr <wesselvonderfuhr@users.noreply.github.com>

--- FAP/Database1/Inspector_shedule.sql
+++ FAP/Database1/Inspector_shedule.sql
@@ -3,5 +3,6 @@
 	[inspector_id] INT NOT NULL PRIMARY KEY, 
     [date] DATE NULL, 
     [available_from] DATETIME NULL, 
-    [available_until] DATETIME NULL
+    [available_until] DATETIME NULL,
+	Foreign Key(inspector_id) References Inspector(id)
 )
