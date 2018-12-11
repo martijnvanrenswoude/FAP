2e52b70983c8de981a3e1e4e354801d7b23350d7
martijnvanrenswoude <mrenswou@avans.nl>
2018-12-11 18:04:46 +0100

    Db ding

---
+++ FAP.Database/db/Tables/QuestionnaireInspector.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[QuestionnaireInspector]
+(
+	[InspectorId] INT NOT NULL, 
+    [QuestionnaireId] INT NOT NULL,
+	PRIMARY KEY (InspectorId, QuestionnaireId),
+	FOREIGN KEY ([InspectorId]) REFERENCES Inspector(Id),
+	FOREIGN KEY ([QuestionnaireId]) REFERENCES Questionnaire(Id)
+)
