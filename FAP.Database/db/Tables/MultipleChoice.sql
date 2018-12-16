315b11283950681ae556ef2e2e53cc62aa79c822
martijnvanrenswoude <mrenswou@avans.nl>
2018-12-16 13:01:05 +0100

    k

--- FAP.Database/db/Tables/MultipleChoice.sql
+++ FAP.Database/db/Tables/MultipleChoice.sql
@@ -2,7 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [questionnaire_id] INT NULL, 
-    [answer] NVARCHAR(MAX) NULL, 
+    [question] NVARCHAR(MAX) NULL, 
     [inspector_id] INT NULL
 	FOREIGN KEY (inspector_id) REFERENCES Inspector(Id)
 	FOREIGN KEY (questionnaire_id) REFERENCES questionnaire(Id),
---
+++ FAP.Desktop/ViewModel/MultichoiceViewModel.cs
@@ -0,0 +1,19 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAP.Desktop.ViewModel
+{
+    class MultichoiceViewModel : ViewModelBase
+    {
+        public string Name { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+
+    }
+}
