42b79f7162e396e3b96408fccfdff82139dadbbe
Henksteenbroek <38421100+Henksteenbroek@users.noreply.github.com>
2018-12-20 14:44:17 +0100

    Added Questionnaire view + repo

---
+++ FAP.Web/Controllers/QuestionnaireController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FAP.Web.Controllers
+{
+    public class QuestionnaireController : Controller
+    {
+        // GET: Vragenlijst
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FAP.Web/Repository/QuestionnaireRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FAP.Web.Repository
+{
+    public class QuestionnaireRepository
+    {
+    }
+}
